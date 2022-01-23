67c0ae2ae2790e25c40349e65dbdfc1947bd36fb
kshayes78 <89166628+kshayes78@users.noreply.github.com>
2022-01-23 14:03:03 -0500

    commit tree 5c3a42a9de95c9daf7df3b90cb6f30c399ddedf2

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-const input = document.getElementById('input')
 function addingEventListener() {
-    input.addEventListener('click', function() {
-    alert('I was clicked!');
-  });
-  
+  const input = document.getElementById("input");
+  function clickAlert() {
+    alert("I was clicked!");
+  }
+  input.addEventListener("click", clickAlert);
 }
-inputEvent.addEventListener("click", clickAlert);
